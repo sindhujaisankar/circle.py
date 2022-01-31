14222112ac64d9f91f0180c3a44ca5500f4e6fe6
sindhujaisankar <97528327+sindhujaisankar@users.noreply.github.com>
2022-01-31 23:02:30 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,3 +1,8 @@
+#area of circle
 r=int(input("enter radius"))
 A=3.14*r*r
 print("The area of circle with radius",r,"is ",A)
+#file extention
+file_name=int(input("enter file name:"))
+file_extention=file_name+.py
+
